4ada9238597e350ff0d92bd7b830f87e4e33a414
Christopher <mottachristopher@edu.unisinos.br>
2024-09-10 23:30:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
 		Department depart = new Department(1, "teste");
-		System.out.println(depart);
+		Seller seller = new Seller(1, "Pedro", "pedro@gmail.com", new Date(), 2020.00, depart);
+		
+		System.out.println(seller);
 	}
 
 }
